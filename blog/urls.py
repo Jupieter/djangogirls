bba58a8e86c5040c2052518773cca215812a4913
Jupieter <63549139+Jupieter@users.noreply.github.com>
2022-05-03 11:59:29 +0200

    finish

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import url
+"""  from django.conf.urls import url    # Django~=1.11.0
 from . import views
 
-urlpatterns = [
+ urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
@@ -12,4 +12,20 @@ urlpatterns = [
     url(r'post/(?P<pk>[0-9]+)/comment/', views.add_comment_to_post, name='add_comment_to_post'),
     url(r'comment/(?P<pk>[0-9]+)/approve/', views.comment_approve, name='comment_approve'),
     url(r'comment/(?P<pk>[0-9]+)/remove/', views.comment_remove, name='comment_remove'),
+] """
+
+from django.urls import path           # Django~=3.2.10
+from . import views
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/(<int:pk>)/edit/$', views.post_edit, name='post_edit'),
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
+    path('post/(<int:pk>)/publish/', views.post_publish, name='post_publish'),
+    path('post/(<int:pk>)/remove/', views.post_remove, name='post_remove'),
+    path('post/(<int:pk>)/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+    path('comment/(<int:pk>)/approve/', views.comment_approve, name='comment_approve'),
+    path('comment/(<int:pk>)/remove/', views.comment_remove, name='comment_remove'),
 ]
